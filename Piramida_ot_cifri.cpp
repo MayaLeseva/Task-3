53cb381e9458975f4a191ad8846ed6d63148998a
MayaLeseva <82907638+MayaLeseva@users.noreply.github.com>
2021-05-26 13:44:39 +0300

    Update Piramida_ot_cifri.cpp

--- Piramida_ot_cifri.cpp
+++ Piramida_ot_cifri.cpp
@@ -1,14 +1,12 @@
 #include<iostream>
-#include<locale>
 using namespace std;
 int main()
 {
-    setlocale (LC_ALL, "bulgarian");
-    cout<<"Въведете брой на линиите, който да е между 5 и 15:";
+    cout<<"Type a number beteen 5 and 15:";
     int num;
     cin>>num;
     if (num<5 or num>15){
-        cout<<"Числото ви не отговаря на зададените граници.";
+        cout<<"Your number is outside the bounds.";
     }
     else{for (int i=num; i>=1;i--){
         for (int h=i; h>=1; h--){
